8b4ee399890e4caf586dffa329bc3b794cc659b7
Robert Loterh <rloterh@gmail.com>
2020-08-06 19:37:02 -0800

    Addmy-each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,10 +1,23 @@
 module Enumerable
   def my_each
-    return to_enum(:my_each) unlessblock_given?
+    return to_enum(:my_each) unless block_given?
     array = to_a.dup
     i = 0
     while i < array.length
       yield array[i]
+      i += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+    array = to_a.dup
+    indexer = 0
+    i = 0
+    while i < array.length
+      yield(array[i], indexer)
+      indexer += 1
       i += 1
     end
     self
